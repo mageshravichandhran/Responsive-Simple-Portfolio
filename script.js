94fe24a5ab5cc40c4b649b8ef0d6c1124ca26d8a
mageshravichandhran <58700838+mageshravichandhran@users.noreply.github.com>
2021-06-26 15:55:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn=document.querySelectorAll(".btn")
+
+btn.addEventListener("click",function(e){
+    e.preventDefault()
+})
+const navbtn=document.querySelectorAll(".navstyle");
+navbtn.addEventListener("click",function(e){
+    e.preventDefault()
+})
